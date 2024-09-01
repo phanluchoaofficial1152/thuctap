c15323a233f3ad478e94ca68eed77eb25d7ac581
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2024-09-01 20:14:26 +0700

    keep logged in status

--- src/app/components/Login/LoginModal.tsx
+++ src/app/components/Login/LoginModal.tsx
@@ -9,11 +9,11 @@ import {
 } from "@ant-design/icons";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/app/store/auth/authSlice";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const LoginModal: FC = () => {
   const [visible, setVisible] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
   const { login, isAuthenticated } = useAuthStore();
 
@@ -25,8 +25,11 @@ const LoginModal: FC = () => {
     setVisible(false);
   };
 
-  const handleLogin = async () => {
-    await login(username, password);
+  const handleLogin = async (values: {
+    username: string;
+    password: string;
+  }) => {
+    await login(values.username, values.password);
     if (isAuthenticated) {
       handleCancel();
       router.push("/");
@@ -37,6 +40,11 @@ const LoginModal: FC = () => {
     handleCancel();
     router.push(url);
   };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+  });
 
   return (
     <>
@@ -55,64 +63,87 @@ const LoginModal: FC = () => {
         width={400}
         className="rounded-lg"
       >
-        <div className="space-y-4">
-          <Input
-            size="large"
-            placeholder="Enter Email"
-            type="email"
-            prefix={<UserOutlined />}
-            className="rounded-md"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Input.Password
-            size="large"
-            placeholder="Enter Password"
-            className="rounded-md"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="primary"
-            block
-            className="rounded-md"
-            onClick={handleLogin}
-          >
-            Sign In
-          </Button>
-          <div className="flex justify-between items-center">
-            <Button
-              type="link"
-              className="text-blue-500 hover:text-blue-700"
-              onClick={() => handleRedirect("/pages/taikhoan/quenmatkhau")}
-            >
-              Forgot Password?
-            </Button>
-            <Button
-              type="link"
-              className="text-blue-500 hover:text-blue-700"
-              onClick={() => handleRedirect("/pages/taikhoan/dangky")}
-            >
-              Register Now
-            </Button>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Button
-              type="default"
-              icon={<FacebookOutlined />}
-              className="rounded-md"
-            >
-              Login with Facebook
-            </Button>
-            <Button
-              type="default"
-              icon={<GoogleOutlined />}
-              className="rounded-md mt-3"
-            >
-              Login with Google+
-            </Button>
-          </div>
-        </div>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          {({ isSubmitting }) => (
+            <Form className="space-y-4">
+              <div>
+                <Field
+                  as={Input}
+                  name="username"
+                  size="large"
+                  placeholder="Enter Email"
+                  type="email"
+                  prefix={<UserOutlined />}
+                  className="rounded-md"
+                />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input.Password}
+                  name="password"
+                  size="large"
+                  placeholder="Enter Password"
+                  className="rounded-md"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+              <Button
+                type="primary"
+                block
+                className="rounded-md"
+                htmlType="submit"
+                disabled={isSubmitting}
+              >
+                Sign In
+              </Button>
+              <div className="flex justify-between items-center">
+                <Button
+                  type="link"
+                  className="text-blue-500 hover:text-blue-700"
+                  onClick={() => handleRedirect("/pages/taikhoan/quenmatkhau")}
+                >
+                  Forgot Password?
+                </Button>
+                <Button
+                  type="link"
+                  className="text-blue-500 hover:text-blue-700"
+                  onClick={() => handleRedirect("/pages/taikhoan/dangky")}
+                >
+                  Register Now
+                </Button>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <Button
+                  type="default"
+                  icon={<FacebookOutlined />}
+                  className="rounded-md"
+                >
+                  Login with Facebook
+                </Button>
+                <Button
+                  type="default"
+                  icon={<GoogleOutlined />}
+                  className="rounded-md mt-3"
+                >
+                  Login with Google+
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
--- src/app/store/auth/authSlice.ts
+++ src/app/store/auth/authSlice.ts
@@ -1,4 +1,3 @@
-import jwt from "jsonwebtoken";
 import { create } from "zustand";
 import Cookies from "js-cookie";
 import crypto from "crypto";
@@ -11,10 +10,14 @@ interface AuthState {
   logout: () => void;
   getDisplayName: () => string | null;
   checkAuthStatus: () => void;
+  refreshToken: () => void;
+  scheduleTokenRefresh: () => void;
 }
 
-const SECRET_KEY: string =
-  "NnhBqVmsDCnZNP4TiX3665fWfQyw+pLblK7TIMHsg7Nrle4mySNhaXRZyHjBPXYNyDY+lnm0rFd5gFJM2klkdhjOyTV9MsAAzq85MqJQL/ViQiIt3vch1A7efKckaS3fOcqOzSV7TYN/awbPIH8/tfa/lI6ByaUgTvYpIUm2Y2pm/LFTD6AIBDyFWwhhnz1cUCxI//CmFy4lfxkJswNdB7vzhoHlgReAdlOeVYwQxjIvRW3rybYuGqGoSF+fBdRR/liFnRg8/oOnAYEl113ik/kUiXWG/u5ApU+9gL9ZwS/vHSXMEi8BqjCd0cDOzZ7oOrffP7t47skDkQkCJSDcRg==";
+const SECRET_KEY: Buffer = crypto
+  .createHash("sha256")
+  .update("ABCDEFGHIERFGJSLFS1234567890@#$%^&*")
+  .digest();
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
@@ -37,51 +40,55 @@ export const useAuthStore = create<AuthState>((set) => ({
       if (response.ok) {
         const data = await response.json();
 
+        const iv = crypto.randomBytes(16);
+        const cipher = crypto.createCipheriv("aes-256-gcm", SECRET_KEY, iv);
+
+        let encrypted = cipher.update(data.data.name, "utf8", "base64");
+        encrypted += cipher.final("base64");
+
+        const mac = cipher.getAuthTag().toString("hex");
+
         const headers = {
-          alg: "HS256",
+          alg: "HS512",
           typ: "JWT",
+          iv: iv.toString("base64"),
+          value: encrypted,
+          mac,
         };
 
-        const payload = {
-          name: String(data.data.name),
+        const encodeBase64Url = (str: string) => {
+          return Buffer.from(str)
+            .toString("base64")
+            .replace(/=/g, "")
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_");
         };
 
-        const encodedHeaders: string = Buffer.from(JSON.stringify(headers))
-          .toString("base64")
-          .replace(/=/g, "")
-          .replace(/\+/g, "-")
-          .replace(/\//g, "_");
-
-        const encodedPayload: string = Buffer.from(JSON.stringify(payload))
-          .toString("base64")
-          .replace(/=/g, "")
-          .replace(/\+/g, "-")
-          .replace(/\//g, "_");
+        const encodedHeaders: string = encodeBase64Url(JSON.stringify(headers));
+        const encodedPayload: string = encodeBase64Url(JSON.stringify({}));
 
         const tokenData: string = `${encodedHeaders}.${encodedPayload}`;
-
-        const hmac = crypto.createHmac("sha256", SECRET_KEY);
-
-        const signature: string = hmac
-          .update(tokenData)
-          .digest("base64")
-          .replace(/=/g, "")
-          .replace(/\+/g, "-")
-          .replace(/\//g, "_");
+        const hmac = crypto.createHmac("sha512", SECRET_KEY);
+        const signature: string = encodeBase64Url(
+          hmac.update(tokenData).digest("base64")
+        );
 
         const token: string = `${tokenData}.${signature}`;
 
-        Cookies.set("access_token", token, { expires: 2 });
+        Cookies.set("access_token", token, { expires: 5 / (24 * 60) });
+
         set({
           isAuthenticated: true,
           token,
           name: data.data.name,
         });
+
+        useAuthStore.getState().scheduleTokenRefresh();
       } else {
-        throw new Error("Login failed");
+        throw new Error("Đăng nhập thất bại!");
       }
     } catch (error) {
-      console.error("Login error:", error);
+      console.error("Đăng nhập thất bại:", error);
     }
   },
 
@@ -92,20 +99,30 @@ export const useAuthStore = create<AuthState>((set) => ({
 
   getDisplayName: () => {
     const token = Cookies.get("access_token");
+
     if (token) {
       try {
         const tokenParts = token.split(".");
         if (tokenParts.length !== 3) {
-          throw new Error("token không đúng format!");
+          throw new Error("Token không đúng format!");
         }
 
-        const encodedPayload = tokenParts[1];
-
-        const payload = JSON.parse(
-          Buffer.from(encodedPayload, "base64").toString("utf-8")
+        const encodedHeaders = tokenParts[0];
+        const headers = JSON.parse(
+          Buffer.from(encodedHeaders, "base64").toString("utf-8")
         );
 
-        return payload.name || null;
+        const decipher = crypto.createDecipheriv(
+          "aes-256-gcm",
+          SECRET_KEY,
+          Buffer.from(headers.iv, "base64")
+        );
+
+        decipher.setAuthTag(Buffer.from(headers.mac, "hex"));
+        let decrypted = decipher.update(headers.value, "base64", "utf8");
+        decrypted += decipher.final("utf8");
+
+        return decrypted || null;
       } catch (error) {
         console.error("Lỗi khi xác minh token:", error);
         return null;
@@ -116,12 +133,93 @@ export const useAuthStore = create<AuthState>((set) => ({
 
   checkAuthStatus: () => {
     const token = Cookies.get("access_token");
+
     if (token) {
-      set({
-        isAuthenticated: true,
-        token,
-        name: useAuthStore.getState().getDisplayName(),
-      });
+      const name = useAuthStore.getState().getDisplayName();
+
+      if (name) {
+        set({
+          isAuthenticated: true,
+          token,
+          name,
+        });
+
+        useAuthStore.getState().scheduleTokenRefresh();
+      } else {
+        useAuthStore.getState().refreshToken();
+      }
+    }
+  },
+
+  refreshToken: () => {
+    const token = Cookies.get("access_token");
+
+    if (token) {
+      try {
+        const tokenParts = token.split(".");
+        if (tokenParts.length !== 3) {
+          throw new Error("Token không đúng format!");
+        }
+
+        const encodedHeaders = tokenParts[0];
+        const headers = JSON.parse(
+          Buffer.from(encodedHeaders, "base64").toString("utf-8")
+        );
+
+        const iv = Buffer.from(headers.iv, "base64");
+        const decipher = crypto.createDecipheriv("aes-256-gcm", SECRET_KEY, iv);
+
+        decipher.setAuthTag(Buffer.from(headers.mac, "hex"));
+        let decrypted = decipher.update(headers.value, "base64", "utf8");
+        decrypted += decipher.final("utf8");
+
+        const cipher = crypto.createCipheriv("aes-256-gcm", SECRET_KEY, iv);
+        let encrypted = cipher.update(decrypted, "utf8", "base64");
+        encrypted += cipher.final("base64");
+
+        const mac = cipher.getAuthTag().toString("hex");
+        const newHeaders = {
+          ...headers,
+          value: encrypted,
+          mac,
+        };
+
+        const encodeBase64Url = (str: string) => {
+          return Buffer.from(str)
+            .toString("base64")
+            .replace(/=/g, "")
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_");
+        };
+
+        const newEncodedHeaders: string = encodeBase64Url(
+          JSON.stringify(newHeaders)
+        );
+
+        const newTokenData: string = `${newEncodedHeaders}.${tokenParts[1]}`;
+        const hmac = crypto.createHmac("sha512", SECRET_KEY);
+        const newSignature: string = encodeBase64Url(
+          hmac.update(newTokenData).digest("base64")
+        );
+
+        const newToken: string = `${newTokenData}.${newSignature}`;
+        Cookies.set("access_token", newToken, { expires: 30 / (24 * 60) });
+
+        set({ token: newToken });
+      } catch (error) {
+        console.error("Lỗi khi refresh token:", error);
+        Cookies.remove("access_token");
+        set({ isAuthenticated: false, token: null, name: null });
+      }
+    }
+  },
+
+  scheduleTokenRefresh: () => {
+    const token = Cookies.get("access_token");
+    if (token) {
+      setTimeout(() => {
+        useAuthStore.getState().refreshToken();
+      }, 4 * 60 * 1000);
     }
   },
 }));

8acb820162700eba07612e8d397b68643a6ae9db
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2024-09-03 22:12:36 +0700

    update upload image

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "pubcdn.ivymoda.com",
-      "cotton4u.vn",
-      "via.placeholder.com",
-      "example.com",
-      "shopsanpham.com",
-      "mint07.com",
-      "encrypted-tbn0.gstatic.com",
-      "images.unsplash.com",
-      "files.fullstack.edu.vn",
-    ],
-  },
+    images: {
+        domains: [
+            "pubcdn.ivymoda.com",
+            "cotton4u.vn",
+            "via.placeholder.com",
+            "example.com",
+            "shopsanpham.com",
+            "mint07.com",
+            "encrypted-tbn0.gstatic.com",
+            "images.unsplash.com",
+            "files.fullstack.edu.vn",
+            "i.ibb.co"
+        ],
+    },
 };
 
 export default nextConfig;
